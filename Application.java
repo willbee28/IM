3578a83f3574c4344acb49876ad1cd885a45fd13
willbee28 <will.t.borwegen@gmail.com>
2018-02-26 13:07:37 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -6,7 +6,11 @@ public class Application extends Thread{
 	//The main function of the server. Starts a new Server that listens on port 7777 and attaches it to a Thread.
 	public static void main(String [] args) throws IOException{
 		
-		new Thread(new Server(7777)).start();
+		try {
+			new Thread(new Server(7777)).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
